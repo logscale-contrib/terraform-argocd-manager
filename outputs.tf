f72c1cc92f37918e824c58978e4b71de87528dd7
Ryan Faircloth <ryan@dss-i.com>
2023-05-13 09:50:38 -0400

    fix: initial release

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "token" {
-    value = kubernetes_secret_v1.argocd_manager_sa.data["token"]
-    #data.kubernetes_secret.argocd_manager.data["token"]
+  sensitive = true
+  value     = kubernetes_secret_v1.argocd_manager_sa.data["token"]
 }

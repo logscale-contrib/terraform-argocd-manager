c6e3b5f4687077bd17bf55d06cc9c9e384df011a
Ryan Faircloth <ryan@dss-i.com>
2023-05-13 09:30:12 -0400

    feat: initial release

---
+++ main.tf
@@ -0,0 +1,55 @@
+
+resource "kubernetes_service_account" "argocd_manager" {
+  metadata {
+    name      = "argocd-manager"
+    namespace = "kube-system"
+  }
+}
+
+resource "kubernetes_cluster_role" "argocd_manager" {
+  metadata {
+    name = "argocd-manager-role"
+  }
+
+  rule {
+    api_groups = ["*"]
+    resources  = ["*"]
+    verbs      = ["*"]
+  }
+
+  rule {
+    non_resource_urls = ["*"]
+    verbs             = ["*"]
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "argocd_manager" {
+  metadata {
+    name = "argocd-manager-role-binding"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = kubernetes_cluster_role.argocd_manager.metadata.0.name
+  }
+
+  subject {
+    kind      = "ServiceAccount"
+    name      = kubernetes_service_account.argocd_manager.metadata.0.name
+    namespace = kubernetes_service_account.argocd_manager.metadata.0.namespace
+  }
+}
+
+resource "kubernetes_secret_v1" "argocd_manager_sa" {
+  metadata {
+    name      = "${kubernetes_service_account.argocd_manager.metadata.0.name}-sagit "
+    namespace = kubernetes_service_account.argocd_manager.metadata.0.namespace
+    annotations = {
+      "kubernetes.io/service-account.name" = kubernetes_service_account.argocd_manager.metadata.0.name
+    }
+  }
+
+  type = "kubernetes.io/service-account-token"
+}
+
